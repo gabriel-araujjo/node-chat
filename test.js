f7edbbe414005839292309d7abc6a4ddae86881e
Gabriel <gabriel140492@gmail.com>
2016-09-30 08:05:02 -0300

    - Fixing some connection events - Changing order on ondata event to on('data', function(data, socket) {}) - Fixing disconnect method

--- test.js
+++ test.js
@@ -6,9 +6,9 @@
 const Chat = require('./lib/chat');
 require('colors');
 
-var chat = Chat();
+var chat = Chat({port: 3355});
 
-function logIncomingMessage(socket, data) {
+function logIncomingMessage(data, socket) {
     var remoteAddressSays = "[" + socket.remoteAddress + "] says";
     console.log(remoteAddressSays.bold);
     console.log('\t' + data.toString());
@@ -19,6 +19,16 @@ function logConnection(socket) {
     console.log(remoteAddressConnected.bold.green);
 }
 
+function logClose(socket) {
+    var remoteAddressConnected = "[" + socket.remoteAddress + "] closed";
+    console.log(remoteAddressConnected.bold.red);
+}
+
+function logBusy(socket) {
+    var remoteAddressConnected = "[" + socket.remoteAddress + "] busy";
+    console.log(remoteAddressConnected.bold.blue);
+}
+
 function logOutcomingMessage(data) {
     chat.write(data);
     console.log('You say:'.bold);
@@ -26,7 +36,9 @@ function logOutcomingMessage(data) {
 }
 
 chat.on('connect', logConnection);
-chat.on('message', logIncomingMessage);
+chat.on('busy', logBusy);
+chat.on('close', logClose);
+chat.on('data', logIncomingMessage);
 
 
 //////////////////////////////////////////////////////
